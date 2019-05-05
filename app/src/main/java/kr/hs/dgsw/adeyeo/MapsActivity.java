a7d943d6307a2cdf76c66c959627b3f88810027a
xeken <xeken@naver.com>
2019-05-05 16:40:19 +0900

    [60%]

--- app/src/main/java/kr/hs/dgsw/adeyeo/MapsActivity.java
+++ app/src/main/java/kr/hs/dgsw/adeyeo/MapsActivity.java
@@ -1,10 +1,14 @@
 package kr.hs.dgsw.adeyeo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
-import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,34 +19,68 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap googleMap;
+    private LocationManager locationManager;
+    private final static int REQUEST_CODE_LOCATION = 37;
+    private ImageButton buttonGoMain2;
+    private ImageButton buttonMyLocation;
+    private SupportMapFragment mapFragment;
 
+    //gps 켜져있는지 확인할 것
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        ImageButton buttonGoMain2 = findViewById(R.id.buttonGoMain2);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        buttonGoMain2 = findViewById(R.id.buttonGoMain2);
         buttonGoMain2.setOnClickListener(v -> finish());
 
-        ImageButton buttonMyLocation = findViewById(R.id.buttonMyLocation);
-        buttonMyLocation.setOnClickListener(v -> {
-            Toast.makeText(this,"현재위치",Toast.LENGTH_SHORT).show();
-        });
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        buttonMyLocation = findViewById(R.id.buttonMyLocation);
+        buttonMyLocation.setOnClickListener(v -> googleMap.moveCamera(CameraUpdateFactory
+                .newLatLng(new LatLng(getMyLocation().getLatitude(), getMyLocation().getLongitude()))));
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap gm) {
 
-        this.googleMap = googleMap;
-        LatLng seoul = new LatLng(37.52487, 126.92723);
+        this.googleMap = gm;
+        Location currentLocation = getMyLocation();
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        googleMap.setOnMapClickListener(v->{
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.title("여기?");
+            markerOptions.position(new LatLng(v.latitude, v.longitude));
+            googleMap.clear();
+            googleMap.addMarker(markerOptions);
+        });
+
         MarkerOptions makerOptions = new MarkerOptions();
         makerOptions
-                .position(seoul)
-                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.");
-        this.googleMap.addMarker(makerOptions); //마커 표시
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul)); //카메라 포커스
+                .position(latLng)
+                .title("현재 위치");
+        googleMap.addMarker(makerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
+
+    private Location getMyLocation() {
+
+        Location currentLocation = null;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION);
+        else {
+            String locationProvider = LocationManager.GPS_PROVIDER;
+            currentLocation = locationManager.getLastKnownLocation(locationProvider);
+        }
+        return currentLocation;
+    }
+
 }
